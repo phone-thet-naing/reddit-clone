c4ac551efb2977908289c04fb9fd52423bc4081a
phone-thet-naing <phonethetnaing34@gmail.com>
2024-09-08 00:56:00 +0630

    eslintrc config changed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
 const authOptions: AuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
@@ -8,14 +9,18 @@ const authOptions: AuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code"
-                }
-            }
+            // authorization: {
+            //     params: {
+            //         prompt: "consent",
+            //         access_type: "offline",
+            //         response_type: "code"
+            //     }
+            // }
         }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || "", 
+            clientSecret: process.env.GITHUB_SECRET || "",
+        })
     ],
 }
 
