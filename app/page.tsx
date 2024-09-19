f904c957c4a5f8563ccf9aa4660a1adfcb0e46d1
peter02092024 <peter@smthgoodco.com>
2024-09-20 00:52:01 +0630

    UI changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import PostListing from "./components/post-listing";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-5 gap-16 font-[family-name:var(--font-geist-sans)]">
-      <header className="w-full bg-orange-300">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-5 gap-2 font-[family-name:var(--font-geist-sans)]">
+      <header className="w-full ">
         <Navbar />
       </header>
-      <main className="bg-slate-400 h-full">
+      <main className="h-full">
         {/* Post section */}
         <PostListing />
       </main>
